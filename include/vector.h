4b4e6ae205042dbc61ca9ae06087476d65e2508b
ehrlz <ehrlz@proton.me>
2026-02-10 23:07:45 +0100

    clang format

--- include/vector.h
+++ include/vector.h
@@ -7,121 +7,114 @@
 namespace dsa
 {
 
-template<typename T> class Vector
+template <typename T> class Vector
 {
 
-public:
+  public:
+    Vector()
+        : size_(0uz), capacity_(50uz), data(nullptr)
+    {
+        data = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
+    }
 
+    ~Vector()
+    {
+        for (auto idx{0uz}; idx < size_; ++idx)
+        {
+            data[idx].~T();
+        }
+        ::operator delete(data);
+    }
 
-	Vector()
-		: size_(0uz)
-		, capacity_(50uz)
-		, data(nullptr)
-	{
-		data = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
-	}
+    // TODO copy constructor
+    // TODO copy assignment
+    // TODO move constructor
+    // TODO move assignment
 
-	~Vector()
-	{
-		for(auto idx{0uz}; idx < size_; ++idx)
-		{
-			data[idx].~T();
-		}
-		::operator delete(data);
-	}
+    // Returns the number of elements in the vector
+    std::size_t size() const
+    {
+        return size_;
+    }
 
-	// TODO copy constructor
-	// TODO copy assignment
-	// TODO move constructor
-	// TODO move assignment
+    // Returns the number of elements that could fit in the reserved memory
+    std::size_t capacity() const
+    {
+        return capacity_;
+    }
 
+    T* swap_data(std::size_t new_capacity)
+    {
+        // reserves new memory
+        T* new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
 
-	// Returns the number of elements in the vector
-	std::size_t size() const
-	{
-		return size_;
-	}
-	
-	// Returns the number of elements that could fit in the reserved memory
-	std::size_t capacity() const
-	{
-		return capacity_;
-	}
-	
-	T	*swap_data(std::size_t new_capacity)
-	{
-		// reserves new memory
-		T *new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
+        // moves or copies each element from the old array
+        for (auto idx{0uz}; idx < size_; ++idx)
+        {
+            try
+            {
+                new (new_data + idx) T(std::move(data[idx]));
+            }
+            catch (...)
+            {
+                // cleans and rethrows
+                for (auto clean_idx{0uz}; clean_idx < idx; ++clean_idx)
+                {
+                    new_data[clean_idx].~T();
+                }
+                ::operator delete(new_data);
+                throw;
+            }
+        }
 
-		// moves or copies each element from the old array
-		for(auto idx{0uz}; idx < size_; ++idx)
-		{
-			try
-			{
-				new (new_data + idx) T(std::move(data[idx]));
-			}
-			catch(...)
-			{
-				// cleans and rethrows
-				for (auto clean_idx{0uz}; clean_idx < idx; ++clean_idx)
-				{
-					new_data[clean_idx].~T();
-				}
-				::operator delete(new_data);
-				throw;
-			}
-		}
-		
-		// cleans the old array
-		for(auto idx{0uz}; idx < size_; ++idx)
-		{
-			data[idx].~T();
-		}
-		::operator delete(data);
-		
-		capacity_ = new_capacity;
-		return new_data;
-	}
-	
-	// Reserves a new slot of memory if more memory is requested. Data is moved or copied
-	void reserve(std::size_t new_capacity)
-	{
-		if (new_capacity <= size_) // no reallocation needed
-		{
-			return;
-		}
-		data = swap_data(new_capacity);
-	}
+        // cleans the old array
+        for (auto idx{0uz}; idx < size_; ++idx)
+        {
+            data[idx].~T();
+        }
+        ::operator delete(data);
 
-	// Adds the element at the end of the list
-	template<typename U>
-	void push_back(const U& elem)
-	{
-    static_assert(std::is_same<T, U>::value, 
-        "push_back argument type must exactly match Vector type");
-		if(size_ == capacity_) // reallocation needed
-		{
-			data = swap_data(size_ * 2);
-		}
-		new (data + size_) T(elem);
-		size_++;
-	}
+        capacity_ = new_capacity;
+        return new_data;
+    }
 
-	// Access an element of the list
-	T at(std::size_t idx)
-	{
-		if(idx >= size_)
-		{
-			throw std::invalid_argument("vector: out of bounds access");
-		}
-		return data[idx];
-	}
+    // Reserves a new slot of memory if more memory is requested.
+    // Data is moved or copied
+    void reserve(std::size_t new_capacity)
+    {
+        if (new_capacity <= size_) // no reallocation needed
+        {
+            return;
+        }
+        data = swap_data(new_capacity);
+    }
 
-private:
-	T *data;								// data stored in the heap
-	std::size_t size_;			// actual elements
-	std::size_t capacity_;	// max reserved capacity
+    // Adds the element at the end of the list
+    template <typename U> void push_back(const U& elem)
+    {
+        static_assert(std::is_same<T, U>::value, "push_back argument type must exactly match Vector type");
+        if (size_ == capacity_) // reallocation needed
+        {
+            data = swap_data(size_ * 2);
+        }
+        new (data + size_) T(elem);
+        size_++;
+    }
+
+    // Access an element of the list
+    T at(std::size_t idx)
+    {
+        if (idx >= size_)
+        {
+            throw std::invalid_argument("vector: out of bounds access");
+        }
+        return data[idx];
+    }
+
+  private:
+    T* data;               // data stored in the heap
+    std::size_t size_;     // actual elements
+    std::size_t capacity_; // max reserved capacity
 };
 
-}
-
+} // namespace dsa
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,75 +1,77 @@
-#include <gtest/gtest.h>
 #include "vector.h"
 
-TEST(tests, vector_constructor) 
+#include <gtest/gtest.h>
+
+TEST(tests, vector_constructor)
 {
-	dsa::Vector<int> vector;
-	EXPECT_EQ(vector.size(), 0uz) << "vector is not empty at construction";
-	EXPECT_GT(vector.capacity(), vector.size()) << "vector capacity shall be greater than size at construction";
+    dsa::Vector<int> vector;
+    EXPECT_EQ(vector.size(), 0uz) << "vector is not empty at construction";
+    EXPECT_GT(vector.capacity(), vector.size()) << "vector capacity shall be greater than size at construction";
 }
 
 TEST(tests, vector_reserve)
 {
-	dsa::Vector<int> vector;
-	auto resize_value{100uz};
-	vector.reserve(resize_value);
-	EXPECT_EQ(vector.size(), 0uz) << "vector resize changes size when it shouldn't";
-	EXPECT_EQ(vector.capacity(), resize_value) << "vector resize doesn't change the capacity correctly";
+    dsa::Vector<int> vector;
+    auto resize_value{100uz};
+    vector.reserve(resize_value);
+    EXPECT_EQ(vector.size(), 0uz) << "vector resize changes size when it shouldn't";
+    EXPECT_EQ(vector.capacity(), resize_value) << "vector resize doesn't change the capacity correctly";
 }
 
 TEST(tests, vector_push_back)
 {
-	dsa::Vector<int> vector;
-	int element_to_insert = 1;
-	vector.push_back(element_to_insert);
-	EXPECT_EQ(vector.size(), 1uz) << "vector push back doesn't increase the size";
-	EXPECT_EQ(vector.at(0), element_to_insert);
+    dsa::Vector<int> vector;
+    int element_to_insert = 1;
+    vector.push_back(element_to_insert);
+    EXPECT_EQ(vector.size(), 1uz) << "vector push back doesn't increase the size";
+    EXPECT_EQ(vector.at(0), element_to_insert);
 }
 
 TEST(tests, vector_push_back_multiple_elements)
-{	
-	dsa::Vector<int> vector;
-	for(int idx{0}; idx < 40; ++idx)
-	{
-		vector.push_back(idx);
-	}
-	for(auto idx{0uz}; idx < 40; ++idx)
-	{
-		EXPECT_EQ(vector.at(idx), idx) << "vector push back doesn't insert properly the elements";
-	}
+{
+    dsa::Vector<int> vector;
+    for (int idx{0}; idx < 40; ++idx)
+    {
+        vector.push_back(idx);
+    }
+    for (auto idx{0uz}; idx < 40; ++idx)
+    {
+        EXPECT_EQ(vector.at(idx), idx) << "vector push back doesn't insert properly the elements";
+    }
 }
 
 TEST(tests, vector_push_back_resize)
 {
-	dsa::Vector<int> vector;
-	for(int idx{0uz}; idx < 75; ++idx)
-	{
-		vector.push_back(idx);
-	}
-	EXPECT_EQ(vector.size(), 75) << "vector push back doesn't increase the size properly";
-	EXPECT_EQ(vector.capacity(), 100) << "vector push back doesn't increase the capacity";
+    dsa::Vector<int> vector;
+    for (int idx{0uz}; idx < 75; ++idx)
+    {
+        vector.push_back(idx);
+    }
+    EXPECT_EQ(vector.size(), 75) << "vector push back doesn't increase the size properly";
+    EXPECT_EQ(vector.capacity(), 100) << "vector push back doesn't increase the capacity";
 }
 
 TEST(tests, vector_at_out_of_bounds)
 {
-	dsa::Vector<int> vector;
-	for(int idx{0uz}; idx < 75; ++idx)
-	{
-		vector.push_back(idx);
-	}
-	EXPECT_THROW(vector.at(76), std::invalid_argument) << "vector at doesn't raises an exception accessing out of bounds";
+    dsa::Vector<int> vector;
+    for (int idx{0uz}; idx < 75; ++idx)
+    {
+        vector.push_back(idx);
+    }
+    EXPECT_THROW(vector.at(76), std::invalid_argument)
+        << "vector at doesn't raises an exception accessing out of bounds";
 }
 
 TEST(tests, vector_at_init_out_of_bounds)
 {
-	dsa::Vector<int> vector;
-	EXPECT_THROW(vector.at(1), std::invalid_argument) << "vector at doesn't raises an exception accessing out of bounds";
+    dsa::Vector<int> vector;
+    EXPECT_THROW(vector.at(1), std::invalid_argument)
+        << "vector at doesn't raises an exception accessing out of bounds";
 }
 
 TEST(tests, vector_at)
 {
-	dsa::Vector<int> vector;
-	vector.push_back(1);
-	EXPECT_NO_THROW(vector.at(0));
+    dsa::Vector<int> vector;
+    vector.push_back(1);
+    EXPECT_NO_THROW(vector.at(0));
 }
-
